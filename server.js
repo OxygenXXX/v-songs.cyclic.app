f8edb56dad312031e24c9f374c34fc43d755df7b
OxygenXXX <oxyyygen.developer@gmail.com>
2022-12-18 17:07:44 +0300

    Express server setup

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require("express");
+
+const application = express();
+
+application.use(express.json());
+application.use(express.urlencoded({ extended: true }));
+
+const server_port = process.env.PORT || 3030;
+
+const server = application.listen(server_port, () =>
+{
+    console.log(`Server port: ${server.address().port}`);
+});
